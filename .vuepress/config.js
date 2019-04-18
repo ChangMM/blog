cd7e7fd9f66b0d3db7d70b674bfa2c9aa42fd6e0
ChangMM <1769111903@qq.com>
2019-04-19 01:33:49 +0800

    [dev]first init

---
+++ .vuepress/config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  base: '/',
+  title: '在下坂本，有何贵干。',
+  port: 8001,
+  description: '一个个人网站，用于记录自己在技术、产品、赚钱、读书、电影、音乐上面的思考与成果。',
+  head: [
+    ['meta', { name: 'description', content: '一个个人网站，用于记录自己在技术、产品、赚钱、读书、电影、音乐上面的思考与成果。'}],
+    ['meta', { name: 'keywords', content: '全栈开发，前端开发，后端开发，技术，产品，赚钱，读书，电影，音乐'}],
+    ['link', { rel: 'icon', href: '/favicon.png'}]
+  ],
+  markdown: {
+   config: md => {
+     md.set({ breaks: true })
+   },
+   anchor: {
+     permalinkSymbol: '¶'
+   },
+  },
+  themeConfig: {
+    logo: '/logo.png',
+    search: false,
+    nav: [
+      { text: '首页', link: '1' },
+      { text: '酷站', link: '2' },
+      { text: '福利', link: '3' },
+      { text: 'Side Project', link: '4' },
+      { text: '关于', link: '5' },
+    ]
+  }
+}
---
+++ article/hello.md
@@ -0,0 +1,12 @@
+---
+author: sakamoto
+tags: [🚀]
+title: Hello, Blog!
+excerpt: 这是第一篇文章
+---
+
+## 测试下导航
+这是第一篇文章
+
+### 参考
+[1] [测试外链](http://ccr.sigcomm.org/online/files/p375-huangA.pdf)
