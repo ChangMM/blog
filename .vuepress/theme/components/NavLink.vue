84210c09ccd7400247c82e5770d47731253a3b99
ChangMM <1769111903@qq.com>
2019-05-07 02:50:05 +0800

    [dev]alter navbar

--- .vuepress/theme/components/NavLink.vue
+++ .vuepress/theme/components/NavLink.vue
@@ -1,10 +1,10 @@
 <template>
   <router-link class="nav-link" :to="link" v-if="!isExternal(link)" :exact="exact">{{ item.text }}</router-link>
-  <a v-else :href="link" class="nav-link external" :target="isMailto(link) || isTel(link) ? null : '_blank'" :rel="isMailto(link) || isTel(link) ? null : 'noopener noreferrer'">{{ item.text }}</a>
+  <a v-else :href="link" class="nav-link external">{{ item.text }}</a>
 </template>
 
 <script>
-import { isExternal, isMailto, isTel, ensureExt } from '../util'
+import { isExternal, ensureExt } from '../util'
 
 export default {
   props: {
@@ -27,9 +27,7 @@ export default {
   },
 
   methods: {
-    isExternal,
-    isMailto,
-    isTel
+    isExternal
   }
 }
 </script>
